539f805433146b5a4284350c3d6179830f52d557
Savanna Bergen <savanna.bergen@gmail.com>
2025-02-23 17:32:03 -0600

    dropdown updated with styled components

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,85 +1,46 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-const StyledUl = styled.ul`
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-`;
-
-const StyledLi = styled.li`
-  float: left;
-`;
-
-const Dropbtn = styled.div`
-  display: inline-block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-`;
-
-const DropDownContent = styled.div`
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-  z-index: 1;
-`;
-
-const DropDownLi = styled(StyledLi)`
-  display: inline-block;
-  &:hover {
-    background-color: red;
-  }
-  &:hover ${DropDownContent} {
-    display: block;
-  }
-`;
-
-const SubA = styled.a`
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-  &:hover {
-    background-color: #f1f9f9;
-  }
-`;
-
 interface DropdownProps {
-  onClick?: (action: string) => void;
+  buttonText: string;
+  menuItems: string[];
   disabled?: boolean;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ onClick }) => {
-  const handleClick = useCallback((action: string) => {
-    if (!action) return;
-    if (onClick) onClick(action);
-  }, [onClick]);
-
+const Dropdown: React.FC<DropdownProps> = ({ buttonText, menuItems }) => {
   return (
-    <StyledUl>
-      <StyledLi>
-        <a onClick={() => handleClick("Home")}>Home</a>
-      </StyledLi>
-      <StyledLi>
-        <a onClick={() => handleClick("News")}>News</a>
-      </StyledLi>
-      <DropDownLi>
-        <Dropbtn>DropDown</Dropbtn>
-        <DropDownContent>
-          <SubA onClick={() => handleClick("Link1")}>Link 1</SubA>
-          <SubA onClick={() => handleClick("Link2")}>Link 2</SubA>
-          <SubA onClick={() => handleClick("Link3")}>Link 3</SubA>
-        </DropDownContent>
-      </DropDownLi>
-    </StyledUl>
+    <StyledDropdown>
+      <StyledButton>{buttonText}</StyledButton>
+      <StyledMenu>
+        {menuItems.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </StyledMenu>
+    </StyledDropdown>
   );
 };
+
+const StyledDropdown = styled.div`
+  position: relative;
+`;
+
+const StyledButton = styled.button`
+  background-color: #333;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+`;
+
+const StyledMenu = styled.div`
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  padding: 10px;
+
+  ${StyledDropdown}:hover & {
+    display: block;
+  }
+`;
 
 export default Dropdown;
--- src/components/dropdown/dropdown.stories.ts
+++ src/components/dropdown/dropdown.stories.ts
@@ -1,10 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Dropdown from "./index";
+import Dropdown from './Dropdown';
 
 const meta = {
-title: "Example/Dropdown",
-component: Dropdown,
-tags: ["docsPage"],
+  title: "Example/Dropdown",
+  component: Dropdown,
+  tags: ["docsPage"],
+  argTypes: {
+    buttonText: { control: { type: "text" } },
+    menuItems: { control: { type: "object" } },
+    },
 } satisfies Meta<typeof Dropdown>;
 
 export default meta;
@@ -12,11 +16,23 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-args: {},
+  args: {
+  buttonText: "Dropdown",
+  menuItems: ["Option 1", "Option 2", "Option 3"],
+  },
+};
+
+export const CustomMenuItems: Story = {
+  args: {
+  buttonText: "Dropdown",
+  menuItems: ["Item 1", "Item 2", "Item 3", "Item 4"],
+  },
 };
 
 export const Disabled: Story = {
-args: {
-disabled: true,
-},
+  args: {
+  buttonText: "Dropdown",
+  menuItems: ["Option 1", "Option 2", "Option 3"],
+  disabled: true,
+  },
 };

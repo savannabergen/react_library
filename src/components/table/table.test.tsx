a3e9e94a8691b253da6a553783a48c9fa1189a23
Savanna Bergen <savanna.bergen@gmail.com>
2025-02-24 17:51:43 -0600

    table tests finished

---
+++ src/components/table/table.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { Table } from './Table';
+import '@testing-library/jest-dom';
+
+describe('Table', () => {
+  test('renders enabled Table with headers and children', async () => {
+    const headers = ['Header 1', 'Header 2'];
+    const children = <tr><td>Cell 1</td><td>Cell 2</td></tr>;
+    render(<Table headers={headers}>{children}</Table>);
+    expect(screen.getByRole('table')).toBeEnabled();
+  });
+
+test('renders disabled Table with headers and children', async () => {
+    const headers = ['Header 1', 'Header 2'];
+    const children = <tr><td>Cell 1</td><td>Cell 2</td></tr>;
+    render(<Table headers={headers} disabled>{children}</Table>);
+    expect(screen.getByRole('table')).toHaveStyle('opacity: 0.5');
+  });
+});

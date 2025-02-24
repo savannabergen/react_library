1fba039d1d2a8d0b637256f7f1578d46cacc84c6
Savanna Bergen <savanna.bergen@gmail.com>
2025-02-24 17:15:08 -0600

    button tests file updated - card tests added

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -6,28 +6,10 @@ describe("Button", () => {
   test("renders enabled button with correct text", async () => {
   render(< Button disabled={false}>Button Text</Button>);
   expect(screen.getByText("Button Text")).toBeInTheDocument();
-});
+  });
 
 test("renders disabled button with correct text", async () => {
   render(<Button disabled>Disabled Button</Button>);
   expect(screen.getByText("Disabled Button")).toBeInTheDocument();
-});
-
-test("applies disabled styles when disabled prop is true", async () => {
-  render(<Button disabled>Disabled Button</Button>);
-  expect(screen.getByText("Disabled Button")).toHaveStyle({
-    opacity: 0.5,
-    pointerEvents: "none",
-    cursor: "not-allowed",
-  });
-});
-
-test("does not apply disabled styles when disabled prop is false", async () => {
-  render(<Button disabled={false}>Button Text</Button>);
-  expect(screen.getByText("Button Text")).not.toHaveStyle({
-    opacity: 0.5,
-    pointerEvents: "none",
-    cursor: "not-allowed",
-    });
   });
 });

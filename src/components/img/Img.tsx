4bdc3bacc628a7b63c1aa2ef50a6bf941c232c79
Savanna Bergen <savanna.bergen@gmail.com>
2025-02-24 17:25:43 -0600

    img tests added

--- src/components/img/Img.tsx
+++ src/components/img/Img.tsx
@@ -7,8 +7,8 @@ const enabledStyles = styled.img`width: 100px; height: 100px; border-radius: 50%
 const disabledStyles = styled.img`width: 100px; height: 100px; border-radius: 50%; opacity: 0.5; pointer-events: none;`
 
 const Img: React.FC<ImgProps> = ({ src, alt, disabled }) => {
-const ImgComponent = disabled ? disabledStyles : enabledStyles;
-return <ImgComponent src={src} alt={alt} />;
+  const ImgComponent = disabled ? disabledStyles : enabledStyles;
+  return <ImgComponent src={src} alt={alt} />;
 };
 
 export default Img;
